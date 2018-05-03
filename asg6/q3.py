e4f366428400e7d5d02d69d2ce5fee5c135efe6e
ernestodiaz95 <erniekid83@gmail.com>
2018-05-03 16:01:08 -0400

    changed final reducer step

--- asg6/q3.py
+++ asg6/q3.py
@@ -4,8 +4,6 @@ from mrjob.job import MRJob
 from mrjob.protocol import JSONValueProtocol
 from mrjob.step import MRStep
 
-productCount = 0
-
 class ProductCategories(MRJob):
 
     # write output as JSON 
@@ -14,24 +12,23 @@ class ProductCategories(MRJob):
     def map_categories(self, _, line):
         line_cols = line.split(',')
         # map categories to None
-        yield line_cols[1], None
+        yield line_cols[1], 1
 
     def combine_categories(self, category, _):
         # combine categories 
-        yield category, None
+        yield category, 1
 
     def reduce_categories(self, category, _):
         # send all (category, _) pairs to the same reducer.
-        yield None, (category, None)
+        yield None, (category, 1)
     
-    def find_total_categories(self, _, category_none_pairs):
-        global productCount
-        # lets get the categories and increment the global counter
+    def find_total_categories(self, _, category_pairs):
+        # lets get the categories by summing up the pairs
         # to find how many there are.
-        for product in category_none_pairs:
-            productCount = productCount + 1
-            yield None, product[0]
+        total = sum(x[1] for x in category_pairs)
         
+        yield None, total
+
     def steps(self):
         return [
             MRStep(mapper=self.map_categories,
@@ -42,6 +39,5 @@ class ProductCategories(MRJob):
         ]
 
 if __name__ == '__main__':
-    print("\nDistinct Product Categories:\n")
+    print("\nDistinct Product Categories: ")
     ProductCategories.run()
-    print ("\nTotal Amount of Categories: {}\n").format(str(productCount))
